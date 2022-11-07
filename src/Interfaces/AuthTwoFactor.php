c7990d8e8183297bb936124e17e839735c36ae4a
teh9 <tengizs.gusevs@gmail.com>
2022-11-07 19:14:02 +0200

    Fixed return type

--- src/Interfaces/AuthTwoFactor.php
+++ src/Interfaces/AuthTwoFactor.php
@@ -4,7 +4,7 @@ namespace Teh9\Laravel2fa\Interfaces;
 
 interface AuthTwoFactor
 {
-    public function setSecretCode (string $lang = 'en', int $codeLength = 6): void;
+    public function setSecretCode (string $lang = 'en', int $codeLength = 6): bool;
 
     public function verifySecret (string $code): bool;
 }
