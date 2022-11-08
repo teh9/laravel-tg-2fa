e9519ba39b7b483415c7b8600873a942aa95f0fe
teh9 <tengizs.gusevs@gmail.com>
2022-11-08 14:13:05 +0200

    Added migrations in service provider

---
+++ src/TelegramTwoFactorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Teh9\Laravel2fa;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelegramTwoFactorServiceProvider extends ServiceProvider
+{
+    public function boot ()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/laravel2fa.php' => config_path('laravel2fa.php'),
+        ], 'config');
+
+        $this->publishMigrations();
+    }
+
+    private function publishMigrations()
+    {
+        $path = $this->getMigrationsPath();
+        $this->publishes([$path => database_path('migrations')], 'migrations');
+    }
+
+    private function getMigrationsPath()
+    {
+        return __DIR__ . '/../database/migrations/';
+    }
+}
